b6deed4fdf2dbbcf8f45a17c88c7ff68e2090f59
Sourish <mukherjee.sourish36@gmail.com>
2026-09-01 02:44:50 +0530

    Apply .gitignore rules

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+#   subnet_id = var.subnet_id_value
+ tags = {
+   Name = "demo_ec2"
+   
+ }
+}
